d65313d3c78366aa6a0d431b398063a15018a8b3
yj256 <yj256@cornell.edu>
2018-03-05 07:47:26 -0500

    finished implementing BoxModel; not yet tested/debugged. BoxController may be merged into GameplayController, therefore incomplete

--- cs3152/core/src/edu/cornell/gdiac/physics/box/BoxController.java
+++ cs3152/core/src/edu/cornell/gdiac/physics/box/BoxController.java
@@ -1,4 +1,107 @@
 package edu.cornell.gdiac.physics.box;
 
+import com.badlogic.gdx.math.*;
+import com.badlogic.gdx.assets.*;
+import com.badlogic.gdx.audio.*;
+import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.g2d.*;
+import com.badlogic.gdx.physics.box2d.*;
+
+import edu.cornell.gdiac.physics.platform.PlatformController;
+import edu.cornell.gdiac.util.*;
+import edu.cornell.gdiac.physics.*;
+import edu.cornell.gdiac.physics.obstacle.*;
+
+/**
+ * Class for box.
+ */
 public class BoxController {
+
+    /** The singleton instance of the box controller */
+    private static BoxController theController = null;
+
+    /**
+     * Return the singleton instance of the box controller
+     *
+     * @return the singleton instance of the box controller
+     */
+    public static BoxController getInstance() {
+        if (theController == null) {
+            theController = new BoxController();
+        }
+        return theController;
+    }
+
+    // Fields
+    /** Whether a box has already been summoned. */
+    private boolean summoned;
+    /** (LATER USAGE) Whether a box is active. */
+    private boolean active;
+    /** Whether a box collided with Annette. */
+    private boolean cAnnette;
+    /** (LATER USAGE) Whether a box collided with a rope. */
+    private boolean cRope;
+    /** (LATER USAGE) Whether a box collided with a creature. */
+    private boolean cCreature;
+
+    public boolean isSummoned() { return summoned; }
+
+    public boolean isActive() { return active; }
+
+    public boolean collidedAnnette() { return cAnnette; }
+
+    public boolean collidedRope() { return cRope; }
+
+    public boolean collidedCreature() { return cCreature; }
+
+    /**
+     * Creates a new box controller.(???)
+     *
+     * The box controller creates a box(???)
+     */
+    public BoxController() {
+        // DONT KNOW WHAT TO DO
+    }
+
+    /**
+     * need: Annette's direction (getDirection),
+     */
+    public void summonBox() {
+        // DONT KNOW WHAT TO DO
+    }
+
+    public void pushBox() {
+        // DONT KNOW WHAT TO DO
+    }
+
+    private void checkCollision() {
+        // DONT KNOW WHAT TO DO
+    }
+
+    /**
+     * Updates that animation for a single box
+     *
+     * This method is here instead of the the box model because of our philosophy
+     * that models should always be lightweight.  Animation includes sounds and other
+     * assets that we do not want to process in the model
+     *
+     * @param  on       Whether to turn the animation on or off
+     */
+    public void updateBox(BoxModel box, boolean on) {
+//        String sound = box.getBoxSound();
+        if (on) {
+            box.animateBox(true);
+            // (3/5/18): sample code; can be used later when we implement sound
+//            if (!SoundController.getInstance().isActive(sound)) {
+//                SoundController.getInstance().play(sound, sound, true);
+//            }
+        } else {
+            box.animateBox(false);
+            // (3/5/18): sample code; can be used later when we implement sound
+//            if (SoundController.getInstance().isActive(sound)) {
+//                SoundController.getInstance().stop(sound);
+//            }
+        }
+    }
+
 }
--- cs3152/core/src/edu/cornell/gdiac/physics/box/BoxModel.java
+++ cs3152/core/src/edu/cornell/gdiac/physics/box/BoxModel.java
@@ -1,4 +1,258 @@
 package edu.cornell.gdiac.physics.box;
 
-public class BoxModel {
+import com.badlogic.gdx.math.*;
+import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.physics.box2d.*;
+
+import edu.cornell.gdiac.util.*;
+import edu.cornell.gdiac.physics.*;
+import edu.cornell.gdiac.physics.obstacle.*;
+
+public class BoxModel extends BoxObstacle {
+
+    // Default physics values
+    /** The density of this box */
+    private static final float DEFAULT_DENSITY = 1.0f;
+    /** The friction of this box */
+    private static final float DEFAULT_FRICTION = 0.0f;
+    /** The restitution of this box */
+    private static final float DEFAULT_RESTITUTION = 0.0f;
+    /** The thrust factor to convert player input into thrust */
+    /** (3/5/18) Not sure if this is needed */
+    /** Random value set as of now */
+    private static final float DEFAULT_THRUST = 15.0f;
+
+    /** The force to apply to this box */
+    private Vector2 force;
+
+    /** The texture filmstrip for the box */
+    FilmStrip mainBox;
+    /** The associated sound for the box (later) */
+    String mainSound;
+    /** The animation phase for the box (later) */
+    boolean mainCycle = true;
+
+    /** Cache object for transforming the force according the object angle */
+    /** Not sure if this is needed as of right now 3/5/2018 */
+    public Affine2 affineCache = new Affine2();
+
+    /**
+     * Returns the force applied to this box.
+     *
+     * This method returns a reference to the force vector, allowing it to be modified.
+     * Remember to modify the input values by the thrust amount before assigning
+     * the value to force.
+     *
+     * @return the force applied to this box.
+     */
+    public Vector2 getForce() {
+        return force;
+    }
+
+    /**
+     * Returns the x-component of the force applied to this box.
+     *
+     * Remember to modify the input values by the thrust amount before assigning
+     * the value to force.
+     *
+     * @return the x-component of the force applied to this box.
+     */
+    public float getFX() {
+        return force.x;
+    }
+
+    /**
+     * Sets the x-component of the force applied to this box.
+     *
+     * Remember to modify the input values by the thrust amount before assigning
+     * the value to force.
+     *
+     * @param value the x-component of the force applied to this box.
+     */
+    public void setFX(float value) {
+        force.x = value;
+    }
+
+    /**
+     * Returns the y-component of the force applied to this box.
+     *
+     * Remember to modify the input values by the thrust amount before assigning
+     * the value to force.
+     *
+     * @return the y-component of the force applied to this box.
+     */
+    public float getFY() {
+        return force.y;
+    }
+
+    /**
+     * Sets the x-component of the force applied to this box.
+     *
+     * Remember to modify the input values by the thrust amount before assigning
+     * the value to force.
+     *
+     * @param value the x-component of the force applied to this box.
+     */
+    public void setFY(float value) {
+        force.y = value;
+    }
+
+    /**
+     * Returns the amount of thrust that this box has.
+     *
+     * Multiply this value times the horizontal and vertical values in the
+     * input controller to get the force.
+     *
+     * @return the amount of thrust that this box has.
+     *
+     * Not sure if this is needed as of 3/5/2018
+     */
+    public float getThrust() {
+        return DEFAULT_THRUST;
+    }
+
+    /**
+     * Creates a new box at the given position.
+     *
+     * The size is expressed in physics units NOT pixels.  In order for
+     * drawing to work properly, you MUST set the drawScale. The drawScale
+     * converts the physics units to pixels.
+     *
+     * @param x  		Initial x position of the box center
+     * @param y  		Initial y position of the box center
+     * @param width		The object width in physics units
+     * @param height	The object width in physics units
+     */
+    public BoxModel(float x, float y, float width, float height) {
+        super(x,y,width,height);
+        force = new Vector2();
+        setDensity(DEFAULT_DENSITY);
+        setDensity(DEFAULT_DENSITY);
+        setFriction(DEFAULT_FRICTION);
+        setRestitution(DEFAULT_RESTITUTION);
+        setName("box");
+
+    }
+
+    /**
+     * Creates the physics Body(s) for this object, adding them to the world.
+     *
+     * This method overrides the base method to keep your box from spinning.
+     *
+     * @param world Box2D world to store body
+     *
+     * @return true if object allocation succeeded
+     */
+    public boolean activatePhysics(World world) {
+        // Get the box body from our parent class
+        if (!super.activatePhysics(world)) {
+            return false;
+        }
+
+        // To prevent the body from rotating
+        setFixedRotation(true);
+
+        return true;
+    }
+
+    /**
+     * Applies the force to the body of this box
+     *
+     * This method should be called after the force attribute is set.
+     *
+     * (3/5/2018) Might need to modify
+     */
+    public void applyForce() {
+        if (!isActive()) {
+            return;
+        }
+
+        // Orient the force with rotation.
+        affineCache.setToRotationRad(getAngle());
+        affineCache.applyTo(force);
+
+        // Apply force to the box BODY, not the box
+        body.applyForceToCenter(force, true);
+    }
+
+    // Animation methods (DO NOT CHANGE)
+    /**
+     * Returns the animation node for this box
+     *
+     * @return the animation node for this box
+     */
+    public FilmStrip getBoxStrip() { return mainBox; }
+
+    /**
+     * Sets the animation node for this box
+     *
+     * @param  strip the animation node for this box
+     */
+    public void setBoxStrip(FilmStrip strip) { mainBox = strip; }
+
+    /**
+     * Returns the key for the sound to accompany this box
+     *
+     * The key should either refer to a valid sound loaded in the AssetManager or
+     * be empty ("").  If the key is "", then no sound will play.
+     *
+     * @return the key for the sound to accompany this box
+     */
+    public String getBoxSound() { return mainSound; }
+
+    /**
+     * Sets the key for the sound to accompany this box
+     *
+     * The key should either refer to a valid sound loaded in the AssetManager or
+     * be empty ("").  If the key is "", then no sound will play.
+     *
+     * @param  key      the key for the sound to accompany this box
+     */
+    public void setBoxSound(String key) { mainSound = key; }
+
+    /**
+     * Animates this box.
+     *
+     * If the animation is not active, it will reset to the initial animation frame.
+     *
+     * @param  on       Whether the animation is active
+     */
+    public void animateBox(boolean on) {
+        FilmStrip node = mainBox;
+        boolean cycle = mainCycle;
+
+        if (on) {
+            // Turn on the flames and go back and forth
+            if (node.getFrame() == 0 || node.getFrame() == 1) {
+                cycle = true;
+            } else if (node.getFrame() == node.getSize()-1) {
+                cycle = false;
+            }
+
+            // Increment
+            if (cycle) {
+                node.setFrame(node.getFrame()+1);
+            } else {
+                node.setFrame(node.getFrame()-1);
+            }
+        } else {
+            node.setFrame(0);
+        }
+
+        mainCycle = cycle;
+    }
+
+    /**
+     * Draws the physics object.
+     *
+     * @param canvas Drawing context
+     */
+    public void draw(GameCanvas canvas) {
+        super.draw(canvas); // Box
+
+        /** (3/5/2018) might need to change the code below; copied from RocketModel */
+        float offsety = mainBox.getRegionHeight()-origin.y;
+        canvas.draw(mainBox,Color.WHITE,origin.x,offsety,getX()*drawScale.x,getY()*drawScale.x,getAngle(),1,1);
+    }
+
 }
