55fbb2d8897f23203a2c26e3923e088e77f867a9
at669 <31416491+at669@users.noreply.github.com>
2018-03-17 22:22:27 -0400

    Changed name, added AnnetteModel

--- B2Lights/core/src/edu/cornell/gdiac/cityoflight/ExteriorModel.java
+++ B2Lights/core/src/edu/cornell/gdiac/cityoflight/ExteriorModel.java
@@ -19,7 +19,7 @@
  * Based on original PhysicsDemo Lab by Don Holden, 2007
  * B2Lights version, 3/12/2016
  */
-package edu.cornell.gdiac.b2lights;
+package edu.cornell.gdiac.cityoflight;
 
 import com.badlogic.gdx.utils.*;
 import com.badlogic.gdx.graphics.*;
