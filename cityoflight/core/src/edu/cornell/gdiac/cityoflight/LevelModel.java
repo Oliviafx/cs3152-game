6fcc8999f2530f2b8d085c990b9df6bca2f2564c
at669 <31416491+at669@users.noreply.github.com>
2018-03-19 02:38:40 -0400

    creatures

--- cityoflight/core/src/edu/cornell/gdiac/cityoflight/LevelModel.java
+++ cityoflight/core/src/edu/cornell/gdiac/cityoflight/LevelModel.java
@@ -348,10 +348,9 @@ public class LevelModel {
         // Create cone lights to be line of sights of creatures.
         createLineofSight(levelFormat.get("vision"));
         // Create the creatures and attach light sources
-        createCreature(levelFormat.get("creatures"), "bob", 0);
-        createCreature(levelFormat.get("creatures"), "fred", 1);
-        createCreature(levelFormat.get("creatures"), "john", 2);
-
+        createCreature(levelFormat.get("creatures"), "snail", 0);
+        createCreature(levelFormat.get("creatures"), "tarasque", 1);
+        createCreature(levelFormat.get("creatures"), "blanche", 2);
 
         // Create box
         box = new BoxModel(1, 1);
@@ -362,7 +361,6 @@ public class LevelModel {
             activate(box);
             box.setActive(true);
         }
-
 
         if (distraction != null) {
             distraction.setAlive(false);
@@ -492,13 +490,17 @@ public class LevelModel {
 	 */
 	public void createCreature(JsonValue creaturejson, String name, int index){
 		CreatureModel creature = new CreatureModel();
-		JsonValue creaturedata = creaturejson.child();
-		//if (creaturedata == null) {System.out.println ("no json found");}
-		creature.initialize(creaturedata);
-		creature.setDrawScale(scale);
-		creatures.add(creature);
-		activate(creature);
-		attachVision(creature, lights.get(index));
+		JsonValue creaturedata = creaturejson.get(name);
+//		while (creaturedata != null) {
+			//if (creaturedata == null) {System.out.println ("no json found");}
+			creature.initialize(creaturedata);
+			creature.setDrawScale(scale);
+			creatures.add(creature);
+			activate(creature);
+			attachVision(creature, lights.get(index));
+//			creaturedata = creaturedata.next();
+//		}
+
 	}
 
 	/**
