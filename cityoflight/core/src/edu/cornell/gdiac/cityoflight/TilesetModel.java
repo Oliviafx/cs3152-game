dcd6779cd77719a8c7a50ff5b5db011bde651c80
yj256 <yj256@cornell.edu>
2018-04-12 01:01:05 -0400

    created models

---
+++ cityoflight/core/src/edu/cornell/gdiac/cityoflight/TilesetModel.java
@@ -0,0 +1,18 @@
+package edu.cornell.gdiac.cityoflight;
+
+public class TilesetModel {
+    public int columns;
+    public int firstgid;
+    public int lastgid; // calculate manually
+    public String image;
+    public int imageHeight;
+    public int imageWidth;
+    public int margin;
+    public String name;
+    public int spacing;
+    public int tileCount;
+    public int tileHeight;
+    public int tileWidth;
+    public int tileAmountWidth; // calculate manually
+    public int transparentColor;
+}
