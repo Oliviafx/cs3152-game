7482d9965def641233d9df5d9989bed973a8532e
Emma Cohn <emc284@cornell.edu>
2018-05-07 20:12:47 -0400

    added level 2

--- cityoflight/core/src/edu/cornell/gdiac/cityoflight/GameController.java
+++ cityoflight/core/src/edu/cornell/gdiac/cityoflight/GameController.java
@@ -361,7 +361,7 @@ public class GameController implements Screen, ContactListener {
 			levelFormat = jsonReader.parse(Gdx.files.internal("jsons/180505_2.json"));
 		}
 		if (whichlevel == 3) {
-			levelFormat = jsonReader.parse(Gdx.files.internal("jsons/Level1.json"));
+			levelFormat = jsonReader.parse(Gdx.files.internal("jsons/Level2.json"));
 		}
 		level.populate(levelFormat);
 		level.getWorld().setContactListener(this);
