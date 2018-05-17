488756c9e2de6769efa035fc06f0435a891dc4e4
yj256 <yj256@cornell.edu>
2018-05-17 05:15:12 -0400

    made new level 4

--- cityoflight/core/src/edu/cornell/gdiac/cityoflight/GameController.java
+++ cityoflight/core/src/edu/cornell/gdiac/cityoflight/GameController.java
@@ -372,7 +372,7 @@ public class GameController implements Screen, ContactListener {
 			levelFormat = jsonReader.parse(Gdx.files.internal("jsons/Level3_5.json"));
 		}
 		if (whichlevel == 4) {
-			levelFormat = jsonReader.parse(Gdx.files.internal("jsons/Level4.json"));
+			levelFormat = jsonReader.parse(Gdx.files.internal("jsons/Level4_5.json"));
 		}
 		if (whichlevel == 5) {
 			levelFormat = jsonReader.parse(Gdx.files.internal("jsons/Level5.json"));
