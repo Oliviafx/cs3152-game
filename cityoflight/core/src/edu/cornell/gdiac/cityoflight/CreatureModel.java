37cd5483185aa83d2452b688f348342025c82add
at669 <31416491+at669@users.noreply.github.com>
2018-05-17 20:56:44 -0400

    snail drawing

--- cityoflight/core/src/edu/cornell/gdiac/cityoflight/CreatureModel.java
+++ cityoflight/core/src/edu/cornell/gdiac/cityoflight/CreatureModel.java
@@ -617,6 +617,16 @@ public class CreatureModel extends BoxObstacle {
                     canvas.draw(dirTexture, Color.WHITE, origin.x, origin.y, (getX() + xOffset) * drawScale.x, getY() * drawScale.y + texture.getRegionHeight() / 6 + dragon_y_offset - 40,
                             0, 0.75f * isReflected, 0.75f);
                 }
+                if (type == 1 && (dirTexture == upAnim || dirTexture == downAnim)) {
+//                    System.out.println("here");
+                    canvas.draw(dirTexture, Color.WHITE, origin.x, origin.y, (getX() + xOffset) * drawScale.x, getY() * drawScale.y + texture.getRegionHeight() / 6 + dragon_y_offset - 20,
+                            0, 0.75f * isReflected, 0.75f);
+                }
+//                else if (type == 1 && dirTexture == downAnim) {
+////                    System.out.println("here");
+//                    canvas.draw(dirTexture, Color.WHITE, origin.x, origin.y, (getX() + xOffset) * drawScale.x, getY() * drawScale.y + texture.getRegionHeight() / 6 + dragon_y_offset + 20,
+//                            0, 0.75f * isReflected, 0.75f);
+//                }
                 else {
                     canvas.draw(dirTexture, Color.WHITE, origin.x, origin.y, (getX() + xOffset) * drawScale.x, getY() * drawScale.y + texture.getRegionHeight() / 6 + dragon_y_offset,
                             0, 0.75f * isReflected, 0.75f);
