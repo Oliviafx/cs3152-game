5310609011102818d99a689885f0de63ed752f43
Emma Cohn <emc284@cornell.edu>
2018-05-18 13:32:49 -0400

    fixed annette's boundary

--- cityoflight/core/src/edu/cornell/gdiac/cityoflight/AnnetteModel.java
+++ cityoflight/core/src/edu/cornell/gdiac/cityoflight/AnnetteModel.java
@@ -562,8 +562,8 @@ public class AnnetteModel extends BoxObstacle {
 
         if (texture != null) {
 //            System.out.println("annette x " + getX() * drawScale.x + " annette y " + getY() * drawScale.y);
-            canvas.draw(shadow, Color.WHITE, origin.x - shadow.getRegionWidth()/4, origin.y - dirTexture.getRegionHeight()/8, getX() * drawScale.x, getY() * drawScale.y, getAngle(), flipped, Math.abs(flipped));
-            canvas.draw(dirTexture, Color.WHITE, origin.x, origin.y - dirTexture.getRegionHeight()/8, getX() * drawScale.x, getY() * drawScale.y, getAngle(), flipped * 0.8f, Math.abs(flipped) * 0.8f);
+            canvas.draw(shadow, Color.WHITE, origin.x - shadow.getRegionWidth()/4, origin.y - dirTexture.getRegionHeight()/4, getX() * drawScale.x, getY() * drawScale.y, getAngle(), flipped, Math.abs(flipped));
+            canvas.draw(dirTexture, Color.WHITE, origin.x, origin.y - dirTexture.getRegionHeight()/4, getX() * drawScale.x, getY() * drawScale.y, getAngle(), flipped * 0.8f, Math.abs(flipped) * 0.8f);
         }
         else{
 //            System.out.println("can't find annette texture");
